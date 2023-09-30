2b349ae21e4d9c621018f06eefe59aaec9b6c1e2
Vibianco <vivi.bianco896@gmail.com>
2023-09-30 20:11:34 -0300

    Se crea pantalla de confirmación de compra. Se añaden tipografías e imágenes en la carpeta res. Pendiente añadir navegabilidad en barra de navegación superior

---
+++ libreria/app/src/main/java/com/proyectoispc/libreria/ConfirmacionCompra.java
@@ -0,0 +1,14 @@
+package com.proyectoispc.libreria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmacionCompra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmacion_compra);
+    }
+}
