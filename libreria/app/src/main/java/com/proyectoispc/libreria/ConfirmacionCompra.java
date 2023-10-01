a69e00976239172d817de3c32602314c04f5126d
Vibianco <vivi.bianco896@gmail.com>
2023-10-01 19:42:21 -0300

    Se modifica layout de confirmación de compra

--- libreria/app/src/main/java/com/proyectoispc/libreria/ConfirmacionCompra.java
+++ libreria/app/src/main/java/com/proyectoispc/libreria/ConfirmacionCompra.java
@@ -10,5 +10,6 @@ public class ConfirmacionCompra extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmacion_compra);
+
     }
 }
