f7b64a63adca6019579b5f42678e8591e2dd4cfe
tefsantana <esantana@fi.uba.ar>
2023-10-02 00:43:11 -0300

    Implementacion de fragmento de Contacto junto a fuente Rubik y se elimina imagen mal importada

--- libreria/app/src/main/java/com/proyectoispc/libreria/MainActivity.java
+++ libreria/app/src/main/java/com/proyectoispc/libreria/MainActivity.java
@@ -15,7 +15,6 @@ import com.proyectoispc.libreria.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_contact)
                 .build();
--- libreria/app/src/main/java/com/proyectoispc/libreria/home.java
+++ libreria/app/src/main/java/com/proyectoispc/libreria/home.java
@@ -1,7 +1,6 @@
 package com.proyectoispc.libreria;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class home extends AppCompatActivity {
