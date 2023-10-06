1d406b29ece4690bb96be1013e5fdfde813c90c1
tefsantana <esantana@fi.uba.ar>
2023-10-05 23:50:56 -0300

    Se agrega navegacion en Carrito para probar que funcione el redireccionamiento y navegacion posterior

--- libreria/app/src/main/java/com/proyectoispc/libreria/Carrito.java
+++ libreria/app/src/main/java/com/proyectoispc/libreria/Carrito.java
@@ -1,7 +1,13 @@
 package com.proyectoispc.libreria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Carrito extends AppCompatActivity {
 
@@ -9,5 +15,41 @@ public class Carrito extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrito);
+
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.nav_view);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if(id == R.id.home){
+                    startActivity(new Intent(getApplicationContext(),Home.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+
+                if(id == R.id.catalogue){
+                    startActivity(new Intent(getApplicationContext(),Catalogue.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+
+                if(id == R.id.contact){
+                    startActivity(new Intent(getApplicationContext(), Contact.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+
+                if(id == R.id.profile){
+                    startActivity(new Intent(getApplicationContext(),Profile.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
