b9f9ca8b081f10e40fefe91749f1db138c4cb771
carlariossuarez <carlaariossuarez@gmail.com>
2023-10-21 20:38:34 -0300

    added credential authentication logic

--- libreria/app/src/main/java/com/proyectoispc/libreria/Login.java
+++ libreria/app/src/main/java/com/proyectoispc/libreria/Login.java
@@ -5,17 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.proyectoispc.libreria.db.DbUser;
 
 public class Login extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText emailInput, passwordInput;
+    DbUser dbUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputLayout emailInputLayout = findViewById(R.id.emailInput);
+        TextInputLayout nameInputLayout = findViewById(R.id.passwordInput);
+        btnLogin = findViewById(R.id.signInButton);
+        dbUser = new DbUser(this);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
     }
 
-    public void login(View view) {
-        Intent intent = new Intent(this, Home.class);
-        startActivity(intent);
+    public void login() {
+
+
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        boolean isAuthenticated = dbUser.checkUserCredentials(email, password);
+
+        if (isAuthenticated) {
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "El usuario y/o la contraseña son incorrectas", Toast.LENGTH_SHORT).show();
+        }
     }
-}
+    }
