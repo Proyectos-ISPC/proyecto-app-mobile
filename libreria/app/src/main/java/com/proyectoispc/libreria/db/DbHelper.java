4031cf456e4c9883993194d20d68af2a88a12918
Vibianco <vivi.bianco896@gmail.com>
2023-10-28 00:27:44 -0300

    Se crea código Java para tabla Delivery en la base de datos

--- libreria/app/src/main/java/com/proyectoispc/libreria/db/DbHelper.java
+++ libreria/app/src/main/java/com/proyectoispc/libreria/db/DbHelper.java
@@ -11,8 +11,8 @@ public class DbHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "library.db";
     public static final String TABLE_USER = "t_user";
-
     public static final String TABLE_SALE = "t_sale";
+    public static final String TABLE_DELIVERY = "t_delivery";
 
 
     private static final String ID = " id INTEGER PRIMARY KEY AUTOINCREMENT,";
@@ -33,6 +33,16 @@ public class DbHelper extends SQLiteOpenHelper {
     private static final String DELIVERY_TYPE = "delivery_type VARCHAR(20) NULL";
     private static final String SALE_DATE = "sale_date date null";
 
+    //Atributos tabla DELIVERY
+
+    private static final String ID_DELIVERY = "id_delivery INTEGER PRIMARY KEY AUTOINCREMENT,";
+    private static final String MAIL = "email VARCHAR(20) NOT NULL,";
+    private static final String TELEPHONE = "phone VARCHAR(20) NOT NULL,";
+    private static final String STREET = "street VARCHAR(20) NULL,";
+    private static final String POSTALCODE = "postalcode VARCHAR(20) NULL,";
+    private static final String LOCATION = "location VARCHAR(20) NULL,";
+    private static final String PROVINCE = "province VARCHAR(20) NULL,";
+
     public DbHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -41,13 +51,14 @@ public class DbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         createUserTable(sqLiteDatabase);
         createSaleTable(sqLiteDatabase);
-
+        createDeliveryTable(sqLiteDatabase);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE " + TABLE_USER);
         sqLiteDatabase.execSQL("DROP TABLE " + TABLE_SALE);
+        sqLiteDatabase.execSQL("DROP TABLE " + TABLE_DELIVERY);
         onCreate(sqLiteDatabase);
     }
 
@@ -60,5 +71,10 @@ public class DbHelper extends SQLiteOpenHelper {
     private void createSaleTable(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_SALE + " (" +
                 ID_SALE + USER_ID + TOTAL_COST + TOTAL_QUANTITY + PAYMENT_TYPE + DELIVERY_TYPE + SALE_DATE + ", FOREIGN KEY(user_id) REFERENCES " + TABLE_USER + "(id))");
+    }
+
+    private void createDeliveryTable(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_DELIVERY + " (" +
+                ID_DELIVERY + MAIL + TELEPHONE + STREET + POSTALCODE + LOCATION + PROVINCE + ", FOREIGN KEY(sale_id) REFERENCES " + TABLE_SALE + "(ID_SALE))");
     }
 }
