d735d70678763bd20b2e67ecaa2ad439d0563e31
MDLDeveloper <mariodanielledesma91@gmail.com>
2023-10-29 02:50:55 -0300

    fix: se arregla bug al momento de crear la tabla delivery

--- libreria/app/src/main/java/com/proyectoispc/libreria/db/DbHelper.java
+++ libreria/app/src/main/java/com/proyectoispc/libreria/db/DbHelper.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 3;
     private static final String DATABASE_NAME = "library.db";
     public static final String TABLE_USER = "t_user";
 
@@ -20,7 +20,7 @@ public class DbHelper extends SQLiteOpenHelper {
     private static final String LAST_NAME = " last_name TEXT,";
     private static final String DNI = " dni TEXT,";
     private static final String ADDRESS = " address TEXT,";
-    private static final String PHONE = " phone TEXT,";
+    private static final String PHONE = " phone TEXT";
 
     public DbHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -37,46 +37,38 @@ public class DbHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE " + TABLE_USER);
-        sqLiteDatabase.execSQL("DROP TABLE delivery");
-        sqLiteDatabase.execSQL("DROP TABLE sale");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS delivery");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS sale");
         onCreate(sqLiteDatabase);
-        createSaleTable(sqLiteDatabase);
-        createDeliveryTable(sqLiteDatabase);
     }
 
 
     private void createUserTable(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_USER + " (" +
-                ID + EMAIL + PASSWORD + NAME + LAST_NAME + DNI + ADDRESS + " phone TEXT)");
+                ID + EMAIL + PASSWORD + NAME + LAST_NAME + DNI + ADDRESS + PHONE + ")");
     }
 
     private void createSaleTable(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(
-                "CREATE TABLE sale (" +
+        sqLiteDatabase.execSQL("CREATE TABLE sale (" +
                         "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         "total_cost DECIMAL(10,2) NOT NULL, " +
                         "total_quantity INTEGER NOT NULL, " +
                         "payment_type VARCHAR(20) NOT NULL, " +
                         "delivery_type VARCHAR(20) NOT NULL, " +
-                        "sale_date DATE, user_id INTEGER, " +
-                        "user_id INTEGER NOT NULL," +
-                        "FOREIGN KEY (user_id) REFERENCES sale(id)" +
-                        "ON DELETE CASCADE)");
+                        "sale_date DATE NOT NULL," +
+                        "user_id INTEGER NOT NULL)");
     }
 
     private void createDeliveryTable(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(
-                "CREATE TABLE delivery(" +
-                        "id PRIMARY KEY AUTOINCREMENT, " +
+        sqLiteDatabase.execSQL("CREATE TABLE delivery (" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         "street VARCHAR(40) NOT NULL, " +
                         "number INTEGER NOT NULL, " +
-                        "postalcode VARCCHAR(10) NOT NULL, " +
+                        "postalcode VARCHAR(10) NOT NULL, " +
                         "status VARCHAR(20) NOT NULL, " +
                         "sale_id INTEGER NOT NULL, " +
                         "fullname VARCHAR(100) NOT NULL, " +
                         "tel VARCHAR(45) NOT NULL, " +
-                        "email VARCHAR(60) NOT NULL, " +
-                        "FOREIGN KEY (sale_id) REFERENCES sale(id)" +
-                        "ON DELETE CASCADE)");
+                        "email VARCHAR(60) NOT NULL)");
     }
 }
